1cea92c6c37defe203b54e1b0307284489ad0204
david-eve-za <david.eve.za@gmail.com>
2025-03-25 17:57:53 -0500

    First Commit

---
+++ src/main/java/com/glez/llm_tools/LlmToolsApplication.java
@@ -0,0 +1,13 @@
+package com.glez.llm_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glez/llm_tools/LlmToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glez.llm_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
